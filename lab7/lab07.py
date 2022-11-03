28d817f8a72e713ec3a75373c7776c6d82041337
giuseppeEsposito98 <giuseppeesposito98@libero.it>
2022-11-03 16:57:44 +0100

    linee diverse per d diversi

--- lab7/lab07.py
+++ lab7/lab07.py
@@ -3,8 +3,6 @@ import random
 from collections import Counter
 import matplotlib.pyplot as plt
 
-#n = int(input("Number of balls and bins: "))
-
 def simulation(n, d = None):
     bins = dict.fromkeys(range(n), 0)
     if d == 1:
@@ -12,9 +10,9 @@ def simulation(n, d = None):
             choice = random.choice(range(n))
             bins[choice] =  bins[choice] +1
     elif d != 1:
-        #d = int(input("Number of selected bins"))
+        possible_choices = np.arange(n)
         for _ in range(n):
-            chosen = random.choices(np.arange(n), k = d)
+            chosen = random.choices(possible_choices, k = d)
             min_ = n
             for idx in chosen:
                 if bins[idx] <= min_:
@@ -23,33 +21,34 @@ def simulation(n, d = None):
             luckiest = random.choice(idxs)
             bins[luckiest] = bins[luckiest] + 1
     counter = Counter(bins.values())
-    #print(counter)
     min_occ = min([val for val in bins.values() if val !=0])
-    print(min_occ)
+    #print(min_occ)
     max_occ = max([val for val in bins.values() if val !=0])
-    print(max_occ)
+    #print(max_occ)
     avg_occ = sum(bins.values())/(n-counter[0])
-    print(avg_occ)
+    #print(avg_occ)
     return max_occ, min_occ, avg_occ
 
 
 max_occs = list()
-min_occs = list()
-avg_occs = list()
-N = [100,1000,10000,10**5, 10**6]
-for n in N:
-    print(n)
-    max_occ, min_occ, avg_occ = simulation(n, 2)
-    max_occs.append(max_occ)
-    min_occs.append(min_occ)
-    avg_occs.append(avg_occ)
+N = [100,1000,10000,10**5]
+D = [1,2,3,4]
+values = list()
+for d in D:
+    for n in N:
+        #print(n)
+        max_occ, min_occ, avg_occ = simulation(n, d)
+        max_occs.append(max_occ)
+    values.append(max_occs)
+    max_occs = list()
+#print("len(values) ", len(values))
 fig = plt.figure()
-plt.plot(N, max_occs)
-plt.plot(N, min_occs)
-plt.plot(N, avg_occs)
+for val in values: 
+    plt.plot(N, val)
 plt.ylabel("occupancy levels")
 plt.xlabel("N")
 plt.show()
+plt.savefig("N-occupancy level.png")
 
 
         
